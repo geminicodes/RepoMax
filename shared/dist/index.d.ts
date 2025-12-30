c8f27e1515b90c7467993de268acc6e9cf06e5fe
Valentin (Val) Mekhonoshina <70847744+geminicodes@users.noreply.github.com>
2025-12-30 11:49:46 +0300

    Delete shared directory

--- shared/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types/analysis";
-export * from "./types/github";
-export * from "./types/job";
-export * from "./types/readme";
-export * from "./types/feedback";
-//# sourceMappingURL=index.d.ts.map
--- shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types/analysis";
-export * from "./types/github";
-export * from "./types/job";
-export * from "./types/readme";
-export * from "./types/feedback";
